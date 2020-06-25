2b1c3d918e9e745aad83042dd207be58fced100c
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-25 20:20:17 +0000

    Adding *kwargs to the base_model

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,27 @@ from datetime import datetime
 
 
 class BaseModel():
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.now())
-        self.updated_at = str(datetime.now())
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == 'updated_at':
+                    self.updated_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now().isoformat()
+            self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = str(datetime.now())
+        self.updated_at = str(datetime.now().isoformat())
 
     def to_dict(self):
         dic = self.__dict__
