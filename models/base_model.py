7f88c164009951ce555659ab0198c197717580a1
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-25 19:17:59 +0000

    Adding the __dict__ representation

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 import uuid
-from datetime import datetime, date
+from datetime import datetime
 
 
 class BaseModel():
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime
-        self.updated_at = datetime
+        self.created_at = str(datetime.now())
+        self.updated_at = str(datetime.now())
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = datetime
+        self.updated_at = str(datetime.now())
+
+    def to_dict(self):
+        dic = self.__dict__
+        dic["__class__"] = self.__class__.__name__
+        return dic
