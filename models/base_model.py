c02a3c48705b883649002b460c04f70d784dd5af
JhoanRodriguez <jhoansebastian@msn.com>
2020-06-28 16:25:10 +0000

    Fixed the date format

--- models/base_model.py
+++ models/base_model.py
@@ -8,25 +8,27 @@ class BaseModel():
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at':
-                    self.created_at = datetime.strptime(
+                    value = datetime.strptime(
                         value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key == 'updated_at':
-                    self.updated_at = datetime.strptime(
+                    value = datetime.strptime(
                         value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != '__class__':
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().isoformat()
-            self.updated_at = datetime.now().isoformat()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
-        self.updated_at = str(datetime.now().isoformat())
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dic = self.__dict__
         dic["__class__"] = self.__class__.__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
         return dic
