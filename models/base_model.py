674bf60bfc40885a1462a00709ffd83cc5619970
samgj18 <samgj_18@hotmail.com>
2020-06-28 12:08:13 -0500

    Changing dict type

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,7 @@ class BaseModel:
 		models.storage.save()
 
 	def to_dict(self):
-		dic = dict(**self.__dict__)
+		dic = self.__dict__
 		dic['__class__'] = str(type(self).__name__)
 		dic['created_at'] = self.created_at.isoformat()
 		dic['updated_at'] = self.updated_at.isoformat()
