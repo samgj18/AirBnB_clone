e6f406356747c56ffd70dfaa5b528d8fac7e8ac6
samgj18 <samgj_18@hotmail.com>
2020-06-30 11:33:34 -0500

    Fixing trailling whitespaces

--- console.py
+++ console.py
@@ -5,74 +5,112 @@ for managing Airbnb files
 """
 import cmd
 import models
-from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-	intro = 'Welcome to the Holberton shell.   Type help or ? to list commands.\n'
-	prompt = '(hbnb) '
+    intro = 'Welcome to Holberton shell. Type help or ? to list commands.\n'
+    prompt = '(hbnb) '
 
-	def do_help(self, args):
-		"""
-		Command lists all help details for each command
-		"""
-		cmd.Cmd.do_help(self, args)
+    def do_help(self, args):
+        """
+        Command lists all help details for each command
+        """
+        cmd.Cmd.do_help(self, args)
 
-	def do_create(self, args):
-		"""
-		Creates a new instance of BaseModel, saves it to JSON file
-		and prints the id
-		"""
-		tokens = args.split()
-		if not args:
-			print("** class name missing **")
-			return
-		if tokens[0] in models.classes:
-			instance = eval(tokens[0] + "()")
-			instance.save()
-			print("{}".format(instance.id))
-		else:
-			print("** class doesn't exist **")
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel, saves it to JSON file
+        and prints the id
+        """
+        tokens = args.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if tokens[0] in models.classes:
+            instance = eval(tokens[0] + "()")
+            instance.save()
+            print("{}".format(instance.id))
+        else:
+            print("** class doesn't exist **")
 
-	def do_show(self, args):
-		"""
-		Show string representation of an instance
-		"""
-		tokens = args.split()
-		objects = models.storage.all()
-		if len(tokens) == 0:
-			print("** class name missing **")
-			return
-		if tokens[0] in models.classes:
-			if len(tokens) < 2:
-				print("** instance id missing **")
-				return
-			else:
-				key = (tokens[0] + "." + tokens[1])
-				try:
-					print(objects[key])
-				except KeyError:
-					print("** no instance found **")
-		else:
-			print("** class doesn't exist **")
+    def do_show(self, args):
+        """
+        Show string representation of an instance
+        """
+        tokens = args.split()
+        objects = models.storage.all()
+        if len(tokens) == 0:
+            print("** class name missing **")
+            return
+        if tokens[0] in models.classes:
+            if len(tokens) < 2:
+                print("** instance id missing **")
+                return
+            else:
+                key = (tokens[0] + "." + tokens[1])
+                try:
+                    print(objects[key])
+                except KeyError:
+                    print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
 
-	@staticmethod
-	def do_quit(args):
-		"""
-		Quit command to exit the program
-		"""
-		quit()
+    def do_all(self, args):
+        """
+        Prints all string representation of all instances
+        based or not on the class name
+        """
+        elements = []
+        objects = models.storage.all()
+        if not args:
+            for element in objects:
+                elements.append(element)
+            print(elements)
+            return
+        tokens = args.split()
+        if tokens[0] in models.classes:
+            print(objects)
+        else:
+            print("** class doesn't exist **")
 
-	@staticmethod
-	def do_EOF(args):
-		"""
-		EOF command to exit the program
-		"""
-		quit()
+    def do_destroy(self, args):
+        """
+        Deletes an instance based on the class name and id
+        saves the changes into JSON file
+        """
+        tokens = args.split()
+        objects = models.storage.all()
+        if len(tokens) == 0:
+            print("** class name missing **")
+            return
+        if tokens[0] in models.classes:
+            if len(tokens) < 2:
+                print("** instance id missing **")
+                return
+            else:
+                key = (tokens[0] + "." + tokens[1])
+                try:
+                    del objects[key]
+                    models.storage.save()
+                except KeyError:
+                    print("** no instance found **")
+        else:
+            print("** class doesn't exist **")
 
-	def do_all(self, args):
-		pass
+    @staticmethod
+    def do_quit(args):
+        """
+        Quit command to exit the program
+        """
+        quit()
+
+    @staticmethod
+    def do_EOF(args):
+        """
+        EOF command to exit the program
+        """
+        quit()
 
 
 if __name__ == "__main__":
-	HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -9,33 +9,34 @@ import models
 
 
 class BaseModel:
-	"""
-	Instantiation of BaseModel
-	"""
+    """
+    Instantiation of BaseModel
+    """
 
-	def __init__(self, *args, **kwargs):
-		if kwargs:
-			for key, value in kwargs.items():
-				if key == 'created_at' or key == 'updated_at':
-					value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-				if key != '__class__':
-					setattr(self, key, value)
-		else:
-			self.id = str(uuid.uuid4())
-			self.created_at = datetime.now()
-			self.updated_at = datetime.now()
-			models.storage.new(self)
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
-	def __str__(self):
-		return "[{}] ({}) {}".format(type(self).__name__, self.id, str(self.__dict__))
+    def __str__(self):
+        return "[{}] ({}) {}".format(type(self).__name__, self.id,
+                                     str(self.__dict__))
 
-	def save(self):
-		self.updated_at = datetime.now()
-		models.storage.save()
+    def save(self):
+        self.updated_at = datetime.now()
+        models.storage.save()
 
-	def to_dict(self):
-		dic = dict(**self.__dict__)
-		dic['__class__'] = type(self).__name__
-		dic['created_at'] = self.created_at.isoformat()
-		dic['updated_at'] = self.updated_at.isoformat()
-		return dic
+    def to_dict(self):
+        dic = dict(**self.__dict__)
+        dic['__class__'] = type(self).__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
